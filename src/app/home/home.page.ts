cf866879593ba78eae0fdcccf08a4f368855546c
Pablo <pablompublic@gmail.com>
2025-01-04 16:00:35 +0100

    Feat: Estilos home y redireccion a login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonImg } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonImg, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
+  private _router: Router = inject(Router);
+  
   constructor() {}
+
+  navigateToLogin() {
+    this._router.navigate(['/login']);
+  }
 }
