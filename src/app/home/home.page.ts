7d22e7cf6c044a0e14ddf3d069c244b60c2ef6e3
Pablo <pablompublic@gmail.com>
2025-01-05 15:39:27 +0100

    Feat: Componente main logo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,14 @@
 import { Component, inject } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonImg } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
+import { LogoComponent } from '../shared/components/logo/logo.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonImg, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, LogoComponent],
 })
 export class HomePage {
   private _router: Router = inject(Router);
