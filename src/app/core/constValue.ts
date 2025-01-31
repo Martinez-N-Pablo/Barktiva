1044069d4d3a20c4d6bd4a0b417feeca3a938b99
Pablo <pablompublic@gmail.com>
2025-01-31 15:44:27 +0100

    Feat: File for constants

---
+++ src/app/core/constValue.ts
@@ -0,0 +1,13 @@
+export const Patterns = {
+    integer: '^[0-9]+$',
+}
+
+export const SexValue = {
+    femenimo: 'f',
+    masculino: 'm',
+}
+
+export const CastratedValue = {
+    castrated: 'c',
+    intact: 'i'
+}
