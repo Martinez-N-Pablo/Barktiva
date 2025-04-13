74bc67323cdfe9abb6954330f1005eaa1570179b
Pablo <pablompublic@gmail.com>
2025-04-13 23:15:40 +0200

    Feat: Confirm delete account

--- src/app/pages/private/user/user.page.ts
+++ src/app/pages/private/user/user.page.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonFooter, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.page.html',
   styleUrls: ['./user.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonButton, IonFooter, IonItem, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonIcon,
+    IonButton, 
+    IonContent,
+    CommonModule,
+    FormsModule,
+  ]
 })
 export class UserPage implements OnInit {
-
+  private _alertController: AlertController = inject(AlertController);
   constructor() { }
 
   ngOnInit() {
@@ -21,5 +28,32 @@ export class UserPage implements OnInit {
 
   redirectToChangePassword() {}
 
-  confirmDeleteAccount() {}
+  /**
+   * * Confirm delete account action with modal
+   */
+  async confirmDeleteAccount() {
+    const alert = await this._alertController.create({
+      header: '¿Estás seguro?',
+      message: 'Esta acción eliminará tu cuenta de forma permanente.',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary'
+        },
+        {
+          text: 'Eliminar',
+          role: 'destructive',
+          handler: () => this.deleteAccount()
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  deleteAccount() {
+    console.log('Cuenta eliminada');
+  }
 }
+
